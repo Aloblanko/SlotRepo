363f0e377bda6f3e3fc712a5d95610b7f9eefc6d
Aloblanko <125378394+Aloblanko@users.noreply.github.com>
2023-06-06 20:26:33 +0200

    version1

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function deductCoins(amount) {
     updateCoinCount();
     return true;
   } else {
-    alert("Not enough coins.");
+   
     return false;
   }
 }
@@ -29,7 +29,7 @@ function updateCoinCount() {
 }
 
 
-const symbols = ['💙', '🌹', '🌸', '🔥'];
+const symbols = ['💙', '🌹', '🌸', '🔥', '🍀', '❼'];
 
 const slotgameElement = document.getElementById("slotgame");
 
@@ -37,6 +37,52 @@ const slotgameElement = document.getElementById("slotgame");
 function randomSymbol() {
   const randomIndex = Math.floor(Math.random() * symbols.length);
   return symbols[randomIndex];
+}
+
+
+
+//alte funktion
+// function playSlot() {
+//   const reel1 = randomSymbol();
+//   const reel2 = randomSymbol();
+//   const reel3 = randomSymbol();
+
+//   const result = `${reel1} | ${reel2} | ${reel3}`;
+
+//   slotgameElement.innerHTML = result;
+
+//   if (reel1 === reel2 && reel2 === reel3) {
+//     addCoins(0)
+//     slotgameElement.style.color = "green";
+//     slotgameElement.style.fontWeight = "bolder";
+//     slotgameElement.textContent += ' - Gewonnen!';
+//   } else {
+//     deductCoins(2);
+//     slotgameElement.style.color = "red";
+//     slotgameElement.style.fontWeight = "bold";
+//     slotgameElement.textContent += ' - Verloren!';
+//   }
+// }
+
+
+
+function calculateWin(symbol1, symbol2, symbol3) {
+  const wins = {
+    "❼❼❼": 250,
+    "💙💙💙": 50,
+    "🌹🌹🌹": 20,
+    "🌸🌸🌸": 20,
+    "🔥🔥🔥": 100,
+    "🍀🍀🍀": 20
+  };
+
+  const winCombination = symbol1 + symbol2 + symbol3;
+
+  if (wins.hasOwnProperty(winCombination)) {
+    return wins[winCombination];
+  } else {
+    return 0;
+  }
 }
 
 function playSlot() {
@@ -48,19 +94,20 @@ function playSlot() {
 
   slotgameElement.innerHTML = result;
 
-  if (reel1 === reel2 && reel2 === reel3) {
-    addCoins(20)
+  const winAmount = calculateWin(reel1, reel2, reel3);
+
+  if (winAmount > 0) {
+    addCoins(winAmount);
     slotgameElement.style.color = "green";
     slotgameElement.style.fontWeight = "bolder";
-    slotgameElement.textContent += ' - Gewonnen!';
+    slotgameElement.textContent += ` (+${winAmount}) `;
   } else {
     deductCoins(2);
     slotgameElement.style.color = "red";
-    slotgameElement.style.fontWeight = "bold";
-    slotgameElement.textContent += ' - Verloren!';
+    slotgameElement.style.fontWeight = "normal";
+    slotgameElement.textContent += " (-2)";
   }
 }
-
 
 
 document.addEventListener("keydown", function(event) {
@@ -69,7 +116,7 @@ document.addEventListener("keydown", function(event) {
     
       playSlot();
     } else {
-      alert("Not enough coins. Minimum 10 coins required to play.");
+      alert("Not enough coins. Minimum 2 coins required to play.");
     }
   }
 });
